7d7f194dbd57c43bdaa39f8146c3f8e3258a87c3
Izripov-Yusuf <izripov1998@gmail.com>
2022-06-16 00:20:15 +0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import './scss/app.scss';
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<MainLayout/>}>
+      <Route path="/" element={<MainLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/pizza/:id" element={<FullPizza />} />
